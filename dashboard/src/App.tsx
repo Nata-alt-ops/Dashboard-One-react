0729a11f33119c0e4f0524866b98f86eeb8b9ec6
n <nataliaklucnikova9@gmail.com>
2025-09-15 11:33:17 +0300

    .

--- dashboard/src/App.tsx
+++ dashboard/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Header } from './header';
+import { Main } from './main';
 
 function App() {
   return (
     <>
     <Header />
+    <Main />
      
     </>
   );
---
+++ dashboard/src/main/Main.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Main = () =>{
+    return(
+       <div className="main_con">
+        
+       </div>
+    )
+}
